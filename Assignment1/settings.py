001a466023e79870c369bea074057d5ef2fbb9b6
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2024-05-21 00:13:55 +1200

    Pushing

--- Assignment1/settings.py
+++ Assignment1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-mo@(k0cs!&du7#!g7c-3k=%k*6i&*gf+b6t6#1kt4n+tukh0xc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
