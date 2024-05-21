76ec96a107754209892c18512601f1213479f9cb
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2024-05-21 12:27:18 +1200

    Pushing

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+# Install pip if it's not available
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found. Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+    rm get-pip.py
+fi
+
+# Install requirements
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
