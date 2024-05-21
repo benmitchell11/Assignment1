5fb1d231dcd6bebc52cb589709c149af05203d2f
benmitchell11 <ben.mitchell11@hotmail.co.nz>
2024-05-21 14:30:41 +1200

    Pushing

--- Assignment1/settings.py
+++ Assignment1/settings.py
@@ -79,14 +79,21 @@ WSGI_APPLICATION = 'Assignment1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gradebook',  # Database name
+#         'USER': 'postgres',  # Username
+#         'PASSWORD': 'Gradebook1',  # Password
+#         'HOST': 'localhost',  # Or the host where your PostgreSQL server is running
+#         'PORT': '5432',  # PostgreSQL default port
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gradebook',  # Database name
-        'USER': 'postgres',  # Username
-        'PASSWORD': 'Gradebook1',  # Password
-        'HOST': 'localhost',  # Or the host where your PostgreSQL server is running
-        'PORT': '5432',  # PostgreSQL default port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
